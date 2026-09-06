be05af7c44d2eeb23b7a925ebd5bbf6d1af02bca
Gaurav Choudhary <49759700+gauravchoudhary1998@users.noreply.github.com>
2026-09-06 14:38:13 +0530

    Time: 3576 ms (6.7%), Space: 33.8 MB (48.16%) - LeetHub

---
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
@@ -0,0 +1,28 @@
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        res = []
+        initialWindow = nums[:k]
+        freq = {}
+        smallest = 0
+        
+        def getXthSmallest(freq, x):
+            count = 0
+            for num in range(-50, 0):
+                count += freq.get(num, 0)
+                if count >= x:
+                    return num
+            return 0
+
+        for num in initialWindow:
+            freq[num] = freq.get(num,0) + 1
+            smallest = getXthSmallest(freq, x)
+
+        res.append(smallest)
+
+        for i in range(k, len(nums)):
+            freq[nums[i-k]] = freq.get(nums[i-k],0) - 1
+            freq[nums[i]] = freq.get(nums[i],0) + 1
+            smallest = getXthSmallest(freq, x)
+            res.append(smallest) 
+
+        return res
